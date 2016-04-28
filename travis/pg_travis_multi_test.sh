4fa4b23ea685ae719a7afb6879da87ec7af8496b
Jason Petersen <jason@citusdata.com>
2016-04-27 21:33:29 -0700

    Permit passing extra tests to multi test script
    Simplest change needed to add custom tests in certain places.

--- travis/pg_travis_multi_test.sh
+++ travis/pg_travis_multi_test.sh
@@ -3,6 +3,8 @@
 set -eux
 
 status=0
+testtargets="check-multi check-multi-fdw check-worker"
+testtargets="${testtargets} $*"
 
 # Configure, build, and install extension
 ./configure PG_CONFIG=/usr/lib/postgresql/$PGVERSION/bin/pg_config
@@ -13,7 +15,7 @@ sudo make install
 cd src/test/regress
 
 # Run tests. DBs owned by non-standard owner put socket in /tmp
-make check-multi check-multi-fdw check-worker || status=$?
+make ${testtargets} || status=$?
 
 # Print diff if it exists
 if test -f regression.diffs; then cat regression.diffs; fi
