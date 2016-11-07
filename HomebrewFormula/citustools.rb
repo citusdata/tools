7c65ed96d4aa4649c30ccad42450957e4fe5a8a4
Jason Petersen <jason@citusdata.com>
2016-11-07 10:40:42 -0700

    Update formula to 0.5.0

--- HomebrewFormula/citustools.rb
+++ HomebrewFormula/citustools.rb
@@ -12,8 +12,8 @@ end
 class Citustools < Formula
   desc "Tools and config used in Citus Data projects."
   homepage "https://github.com/citusdata/tools"
-  url "https://github.com/citusdata/tools/archive/v0.4.2.tar.gz"
-  sha256 "b12706bd3dc88d788711c3beebddf377710e9c9ab0ba7207634d5eaa1b118865"
+  url "https://github.com/citusdata/tools/archive/v0.5.0.tar.gz"
+  sha256 "dd0eb39686f7c2cd61084ed3bbf52ffe7ea6fe60d715b6bf4ca3a7b775c30ec9"
 
   depends_on "uncrustify"
   depends_on Docker
