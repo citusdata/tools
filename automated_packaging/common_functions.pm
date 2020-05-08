b7dfe94e73aa851800c535f2f5746b67f7ae0170
Onur Tirtir <onurcantirtir@gmail.com>
2020-05-08 15:14:58 +0300

    Give warning for descriptions >78 instead of killing process

--- automated_packaging/common_functions.pm
+++ automated_packaging/common_functions.pm
@@ -150,11 +150,12 @@ sub create_release_changelog {
             foreach $line (@log_output) {
                 if ($line =~ /^DESCRIPTION: */) {
                     $description_part = substr($line, length($&), -1);
+                    
                     if (length($description_part) > 78) {
-                        print("You have to shorten PR message $description_part of $pr_url");
-                        `git reset --hard`;
-                        die "Can not add description longer than 78 charachters";
+                        print("You have to shorten PR message $description_part of $pr_url\n");
+                        print("Description should not be longer than 78 charachters, please manually shorten this description\n");
                     }
+
                     print("Description $description_part has been added ... \n");
                     push(@comment_lines, "* " . $description_part . "\n\n");
                 }
