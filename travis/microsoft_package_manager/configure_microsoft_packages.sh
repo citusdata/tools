5b7ba64ff7981b6d326405a0bfc11a0d3eb49ccd
gindibay <gurkanindibay@gmail.com>
2021-03-09 18:12:45 +0300

    add scripts to push packages into Microsoft Packages

---
+++ travis/microsoft_package_manager/configure_microsoft_packages.sh
@@ -0,0 +1,29 @@
+set -euxo pipefail
+git checkout repoclient
+sudo apt-get install python3-openssl python3-adal jo alien
+sudo dpkg -i azure-repoapi-client_1.0.5-beta_amd64.deb
+
+mkdir -p ~/.repoclient
+# Uses package-repo-service.trafficmanager.net instead of
+# package-repo-service.corp.microsoft.com, because that one cano
+# only be resolved on corpnet
+cat > ~/.repoclient/config.json<< EOD
+{
+"server": "package-repo-service.trafficmanager.net",
+"port": "443",
+"AADClientId": "fed01f70-081c-4e14-abed-b15a00c1fcac",
+"AADClientSecret": "${REPO_CLIENT_SECRET}",
+"AADResource": "https://microsoft.onmicrosoft.com/945999e9-da09-4b5b-878f-b66c414602c0",
+"AADTenant": "72f988bf-86f1-41af-91ab-2d7cd011db47",
+"AADAuthorityUrl": "https://login.microsoftonline.com",
+"repositoryId": "something-needs-to-be-here"
+}
+EOD
+#cat ~/.repoclient/config.json
+#repoclient repo list | jq '.[] | select(.url | startswith("citus"))'
+
+# Fix a bug in repoclient
+sudo sed 's/resp.status_code != 200/resp.status_code >= 300/' -i /usr/lib/python3/dist-packages/azure/repoclient/repolib.py
+
+# Import microsoft key to hide warnings
+curl https://packages.microsoft.com/keys/microsoft.asc > microsoft.asc && sudo rpm --import microsoft.asc
---
+++ travis/microsoft_package_manager/publish-packages.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+from subprocess import PIPE
+import subprocess
+import os
+import json
+import re
+import time
+from pprint import pprint
+
+
+def run(command, *args, **kwargs):
+    print(command)
+    result = subprocess.run(command, *args, check=True, shell=True, **kwargs)
+    print()
+    return result
+
+
+result = run("repoclient repo list", stdout=PIPE)
+
+all_repos = json.loads(result.stdout)
+
+citus_repos = {}
+for repo in all_repos:
+    if not repo["url"].startswith("citus-"):
+        continue
+    name = repo["url"][len("citus-") :]
+    if name in ("ubuntu", "debian"):
+        # Suffix distribution
+        name = name + "-" + repo["distribution"]
+    else:
+        # Put dash before numberrepo
+        name = re.sub(r"(\d+)", r"-\1", name)
+    citus_repos[name] = repo
+
+pprint(citus_repos)
+
+
+submission_responses = {}
+
+for platform in os.listdir("signed-packages"):
+    print(platform)
+    repo = citus_repos[platform]
+    pprint(citus_repos[platform])
+    platform_dir = os.path.join("signed-packages", platform)
+    for package in os.listdir(platform_dir):
+        package_path = os.path.join(platform_dir, package)
+
+        # Ensure deb packages contain the distribution, so they do not conflict
+        if repo["url"] in ("citus-ubuntu", "citus-debian"):
+            if repo["distribution"] not in package:
+                if not package_path.endswith("amd64.deb"):
+                    raise "Package should have ended with amd64.deb: %s" % package_path
+                old_package_path = package_path
+                package_prefix = package_path[: -len("amd64.deb")]
+                package_path = "%s+%s_amd64.deb" % (
+                    package_prefix,
+                    repo["distribution"],
+                )
+                os.rename(old_package_path, package_path)
+
+        # Publish packages
+        result = run(
+            "repoclient package add --repoID %s %s" % (repo["id"], package_path),
+            stdout=PIPE,
+        )
+        submission_responses[package_path] = json.loads(result.stdout)
+        print(
+            "Waiting for 30 seconds to avoid concurrency problems on publishing server"
+        )
+        time.sleep(30)
+
+pprint(submission_responses)
+
+# Check if published successfully
+for i in range(15):
+    unfinished_submissions = {}
+    for package_path, response in submission_responses.items():
+        package_id = response["Location"].split("/")[-1]
+
+        try:
+            run("repoclient package check %s" % package_id)
+        except Exception:
+            print(package_path, "was not finished publishing")
+            unfinished_submissions[package_path] = response
+
+    if not unfinished_submissions:
+        break
+    submission_responses = unfinished_submissions
+    time.sleep(i)
+
+if unfinished_submissions:
+    print("The following packages were not published successfuly")
+    pprint(unfinished_submissions)
+    raise Exception("Some packages were not finished publishing")
