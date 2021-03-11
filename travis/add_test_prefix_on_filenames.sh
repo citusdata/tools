0ca2eea32bac8e33cd84b51ffdc5312f974b38ff
gindibay <gurkanindibay@gmail.com>
2021-03-11 13:58:25 +0300

    remove apt-get

--- travis/add_test_prefix_on_filenames.sh
+++ travis/add_test_prefix_on_filenames.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
- for FILE in ./pkgs/releases/*;do
+ for FILE in $(find ./pkgs/releases -type f );do
    file_name="${FILE##*/}"
    path="${FILE%/*}"
    test_file_name="test_${file_name}"
    echo "Processing renaming on file ${path}/${test_file_name}"
    mv  "${FILE}" "${path}/${test_file_name}"
  done
+
