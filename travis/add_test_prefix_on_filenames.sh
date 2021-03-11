6a5e1e8867bd6dfaeeaaa77494585ab3ceedae63
gindibay <gurkanindibay@gmail.com>
2021-03-11 10:41:41 +0300

    fix path

--- travis/add_test_prefix_on_filenames.sh
+++ travis/add_test_prefix_on_filenames.sh
@@ -4,6 +4,6 @@
    file_name="${FILE##*/}"
    path="${FILE%/*}"
    test_file_name="test_${file_name}"
-   echo "Processing reniaming on file ${path}/${test_file_name}"
+   echo "Processing renaming on file ${path}/${test_file_name}"
    mv  "${FILE}" "${path}/${test_file_name}"
  done
--- travis/configure_microsoft_packages.sh
+++ travis/configure_microsoft_packages.sh
@@ -1,12 +1,23 @@
 set -euxo pipefail
+
+while getopts p: flag; do
+  case "${flag}" in
+  p) PATH=${OPTARG} ;;
+  *) ;;
+  esac
+done
+
+echo "File: ${PATH}"
+[ -z "${PATH}" ] && echo "File Path should not be empty Usage ./configure_microsoft_packages.sh -p <file-path>" && exit 1
+
 sudo apt-get install python3-openssl python3-adal jo alien
-sudo dpkg -i azure-repoapi-client_1.0.5-beta_amd64.deb
+sudo dpkg -i "${PATH}/azure-repoapi-client_1.0.5-beta_amd64.deb"
 
 mkdir -p ~/.repoclient
 # Uses package-repo-service.trafficmanager.net instead of
 # package-repo-service.corp.microsoft.com, because that one cano
 # only be resolved on corpnet
-cat > ~/.repoclient/config.json<< EOD
+cat >~/.repoclient/config.json <<EOD
 {
 "server": "package-repo-service.trafficmanager.net",
 "port": "443",
@@ -25,4 +36,4 @@ EOD
 sudo sed 's/resp.status_code != 200/resp.status_code >= 300/' -i /usr/lib/python3/dist-packages/azure/repoclient/repolib.py
 
 # Import microsoft key to hide warnings
-curl https://packages.microsoft.com/keys/microsoft.asc > microsoft.asc && sudo rpm --import microsoft.asc
+curl https://packages.microsoft.com/keys/microsoft.asc >microsoft.asc && sudo rpm --import microsoft.asc
