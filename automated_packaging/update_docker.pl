68eea3c8d0093d30d60911a6d8e952ce43ac60cb
gindibay <gurkanindibay@gmail.com>
2021-03-19 08:02:42 +0300

    fix the comment for postgres 12 docker file section

--- automated_packaging/update_docker.pl
+++ automated_packaging/update_docker.pl
@@ -34,7 +34,7 @@ if ( $num_args == 2 ) {
 `sed -i 's/VERSION=[[:digit:]]*.[[:digit:]]*.[[:digit:]]*/VERSION=$VERSION/g' Dockerfile-alpine`;
 `sed -i 's/PG_MAJOR-citus-[[:digit:]]*.[[:digit:]]*/PG_MAJOR-citus-$minor_version/g' Dockerfile-alpine`;
 
-# Update citus version on alpine Dockerfile
+# Update citus version on Postgres 12 Dockerfile
 `sed -i 's/VERSION=[[:digit:]]*.[[:digit:]]*.[[:digit:]]*/VERSION=$VERSION/g' postgres-12/Dockerfile`;
 `sed -i 's/PG_MAJOR-citus-[[:digit:]]*.[[:digit:]]*/PG_MAJOR-citus-$minor_version/g' postgres-12/Dockerfile`;
 
