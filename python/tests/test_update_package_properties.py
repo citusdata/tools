425305f91fa4bdbaa4837129f972993e0955cd15
Gürkan İndibay <gindibay@microsoft.com>
2021-03-31 18:00:51 +0300

    Initial test pipeline
    Fix path problem
    
    Add update_package_properties and its unit tests
    
    Remove github token from scripts
    
    Remove unattended upgrades since gives error
    
    Remove unneccessary pkgvars file
    
    Fix requirements.txt path
    
    Update tool-tests.yml
    
    Update requirements path
    
    Update test path
    
    Add missing requirements
    
    Remove missing package from requirements
    
    Fix Paths for test
    Move changelog into test dir
    
    Install python 3.9
    
    Install python 3.9
    
    Change removeprefix and removesuffix since this features are python 3.9 features but GithubActions environment is 3.8.5
    
    Add validation for GITHUB_TOKEN
    
    Fix validation for GITHUB_TOKEN
    
    Add parameter GH_TOKEN into pipeline yml
    
    Remove logs and fix name for the last task
    
    Fix some static code warnings
    Add checks to prevent
    Add checks and test cases to prevent multiple changelog add
    
    Fix missing parameter problems on update_al_changes
    
    Fix bash command
    
    Add update package properties
    
    Add flags to make script fail when a step fail
    
    remove parameter set on update_package_properties.sh to get these parameters from pipeline parameters
    
    Remove unnecessary files
    
    Change pipeline name
    Add trigger to run on all branches and pr requests
    
    Add pycache files into gitignore
    
    Remove useless files
    Set default variables on update_package_properties.sh
    to prevent "unbound variable" error
    
    Simplified git ignore of pycache
    
    Co-authored-by: Jelte Fennema <github-tech@jeltef.nl>
    
    Removed useless comments on pipeline file
    Simplify string join blocks on update_package_properties.py
    
    Add missing emptylines
    
    Transform long methpod parameters into class
    
    Use getenv default values handling logic
    Remove unnecessary comments
    Change pipeline job name
    Add new line at the end of some files
    
    Add new line at the end of some files
    Change test framework into pytest
    
    Add missing dependencies for pycurl
    
    Remove unnecessary package unattended-upgrades
    
    Add pytest execution command on pipeline file
    
    Add __init__.py files for module definitions
    
    Add validation controls into ChangelogParams
    setters
    Remove unused code blocks
    
    Change the release_date datatype into datetime to
    match the internal data type
    
    Fix spec file rpm version problem which causes
    build fail
    
    Fix missing project name problem on rpm spec
    
    Remove empty character that cause tests fail
    from rpm_changelog converter
    
    Fix pkgvars version style
    
    Fix review notes on PR #90
    
    Fix python build error
    
    Fixed review notes on PR #90
    
    Fixed python build error related to sos 4.0
    
    Fix for @Jelte's comments on PR #90
    
    Update tool-tests.yml
    
    Update tool-tests.yml
    
    Update name and trigger condition
    
    Add Trigger condition to run on all branches
    
    Fixed comments of @Hanefi on PR #90
    
    Fix for PR #90 reviews of @hanefi
    * Change update_all_changes signature using PackagePropertiesParams
    * Fix some capitilization issues
    * Fix string emtpiness validation checks
    
    Add make and make install as a job into test
    pipeline

---
+++ python/tests/test_update_package_properties.py
@@ -0,0 +1,208 @@
+import pytest
+import os
+from shutil import copyfile
+
+from .test_utils import are_strings_equal
+from ..update_package_properties import *
+
+TEST_BASE_PATH = pathlib2.Path(__file__).parent.absolute()
+BASE_PATH = os.getenv("BASE_PATH", default=pathlib2.Path(__file__).parents[1])
+GITHUB_TOKEN = os.getenv("GH_TOKEN")
+PROJECT_VERSION = os.getenv("PROJECT_VERSION", default="10.0.3")
+TAG_NAME = os.getenv("TAG_NAME", default="v10.0.3")
+PROJECT_NAME = os.getenv("PROJECT_NAME", default="citus")
+MICROSOFT_EMAIL = os.getenv("MICROSOFT_EMAIL", default="gindibay@microsoft.com")
+NAME_SURNAME = os.getenv("NAME_SURNAME", default="Gurkan Indibay")
+CHANGELOG_DATE = datetime.strptime('Thu, 18 Mar 2021 01:40:08 +0000', '%a, %d %b %Y %H:%M:%S %z') if os.getenv(
+    "CHANGELOG_DATE") is None else datetime.strptime(os.getenv("CHANGELOG_DATE"), '%a, %d %b %Y %H:%M:%S %z')
+
+
+def default_changelog_param_for_test(latest_changelog, changelog_date):
+    changelog_param = PackagePropertiesParams(project_name=PROJECT_NAME, project_version=PROJECT_VERSION, fancy=True,
+                                              fancy_version_number=1, microsoft_email=MICROSOFT_EMAIL,
+                                              name_surname=NAME_SURNAME, changelog_date=changelog_date,
+                                              latest_changelog=latest_changelog)
+    return changelog_param
+
+
+DEFAULT_CHANGELOG_PARAM_FOR_TEST = default_changelog_param_for_test("", CHANGELOG_DATE)
+
+
+def test_get_version_number():
+    assert DEFAULT_CHANGELOG_PARAM_FOR_TEST.version_number() == "10.0.3-1"
+
+
+def test_get_version_number_with_project_name():
+    assert DEFAULT_CHANGELOG_PARAM_FOR_TEST.version_number_with_project_name() == "10.0.3.citus-1"
+
+
+def test_find_nth_overlapping():
+    assert find_nth_overlapping("foofoo foofoo", "foofoo", 2) == 7
+
+
+def test_get_changelog_for_tag():
+    changelog = changelog_for_tag(GITHUB_TOKEN, "citus", "v10.0.3")
+    with open(f"{TEST_BASE_PATH}/files/verify/expected_changelog_10.0.3.txt", "r") as reader:
+        expected_changelog = reader.read()
+    assert expected_changelog == changelog
+
+
+def test_get_debian_changelog_header():
+    header = debian_changelog_header("### citus v10.0.3 (March 16, 2021) ###", True, 2)
+    assert header == "citus (10.0.3.citus-2) stable; urgency=low"
+
+
+def test_get_last_changelog_from_debian():
+    refer_file_path = f"{TEST_BASE_PATH}/files/verify/debian_changelog_with_10.0.3.txt"
+    expected_file_path = f"{TEST_BASE_PATH}/files/verify/expected_debian_latest_v10.0.3.txt"
+    with open(refer_file_path, "r") as reader:
+        changelog = reader.read()
+
+    latest_changelog = get_last_changelog_content_from_debian(changelog)
+
+    with open(expected_file_path, "r") as reader:
+        expected_output = reader.read()
+
+    are_strings_equal(expected_output, latest_changelog)
+
+
+def test_prepend_latest_changelog_into_debian_changelog():
+    refer_file_path = f"{TEST_BASE_PATH}/files/debian.changelog.refer"
+    changelog_file_path = f"{TEST_BASE_PATH}/files/debian.changelog"
+    copyfile(refer_file_path, changelog_file_path)
+    latest_changelog = changelog_for_tag(GITHUB_TOKEN, PROJECT_NAME, TAG_NAME)
+
+    changelog_param = default_changelog_param_for_test(latest_changelog, CHANGELOG_DATE)
+
+    try:
+        prepend_latest_changelog_into_debian_changelog(changelog_param, changelog_file_path)
+        verify_prepend_debian_changelog(changelog_file_path)
+    finally:
+        os.remove(changelog_file_path)
+
+
+def test_prepend_latest_changelog_into_debian_changelog_10_0_3_already_included():
+    refer_file_path = f"{TEST_BASE_PATH}/files/debian.changelog_include_10_0_3.refer"
+    changelog_file_path = f"{TEST_BASE_PATH}/files/debian.changelog"
+    copyfile(refer_file_path, changelog_file_path)
+    latest_changelog = changelog_for_tag(GITHUB_TOKEN, PROJECT_NAME, TAG_NAME)
+    changelog_param = default_changelog_param_for_test(latest_changelog, datetime.now())
+    try:
+
+        with pytest.raises(ValueError):
+            prepend_latest_changelog_into_debian_changelog(changelog_param, changelog_file_path)
+    finally:
+        os.remove(changelog_file_path)
+
+
+def verify_prepend_debian_changelog(changelog_file_path):
+    with open(changelog_file_path, "r") as reader:
+        content = reader.read()
+        latest_changelog = get_last_changelog_content_from_debian(content)
+    with open(f"{TEST_BASE_PATH}/files/verify/expected_debian_latest_v10.0.3.txt", "r") as reader:
+        expected_content = reader.read()
+    are_strings_equal(expected_content, latest_changelog)
+
+
+def test_convert_citus_changelog_into_rpm_changelog():
+    changelog_param = default_changelog_param_for_test("", CHANGELOG_DATE)
+    changelog = convert_citus_changelog_into_rpm_changelog(changelog_param)
+    with open(f"{TEST_BASE_PATH}/files/verify/rpm_latest_changelog_reference.txt", "r") as reader:
+        content = reader.read()
+    assert content == changelog
+
+
+def test_update_rpm_spec():
+    project_name = "citus"
+    spec_file = f"{TEST_BASE_PATH}/files/{spec_file_name(project_name)}"
+    spec_file_copy = f"{os.getcwd()}/{spec_file_name(project_name)}_copy"
+    spec_file_reference = f"{TEST_BASE_PATH}/files/citus_include_10_0_3.spec"
+    templates_path = f"{BASE_PATH}/templates"
+    copyfile(spec_file, spec_file_copy)
+    try:
+        changelog_param = default_changelog_param_for_test("", CHANGELOG_DATE)
+        update_rpm_spec(changelog_param, spec_file, templates_path)
+        verify_rpm_spec(spec_file_reference, spec_file)
+    finally:
+        copyfile(spec_file_copy, spec_file)
+        os.remove(spec_file_copy)
+
+
+def test_update_rpm_spec_include_10_0_3():
+    project_name = "citus"
+    spec_file = f"{TEST_BASE_PATH}/files/citus_include_10_0_3.spec"
+    spec_file_copy = f"{os.getcwd()}/{spec_file_name(project_name)}_copy"
+    templates_path = f"{BASE_PATH}/templates"
+    copyfile(spec_file, spec_file_copy)
+    try:
+        changelog_param = default_changelog_param_for_test("", CHANGELOG_DATE)
+        with pytest.raises(ValueError):
+            update_rpm_spec(changelog_param, spec_file, templates_path)
+    finally:
+        copyfile(spec_file_copy, spec_file)
+        os.remove(spec_file_copy)
+
+
+def verify_rpm_spec(spec_file_reference, spec_file_for_test):
+    with open(spec_file_for_test, "r") as reader_test:
+        with open(spec_file_reference, "r") as reader_reference:
+            test_str = reader_test.read()
+            reference_str = reader_reference.read()
+            are_strings_equal(reference_str, test_str)
+
+
+def test_update_pkg_vars():
+    templates_path = f"{BASE_PATH}/templates"
+    pkgvars_path = f"{TEST_BASE_PATH}/files/pkgvars"
+    pkgvars_copy_path = f"{pkgvars_path}_copy"
+    copyfile(pkgvars_path, pkgvars_copy_path)
+    try:
+        update_pkgvars(PROJECT_NAME, PROJECT_VERSION, True, 1, templates_path, f"{TEST_BASE_PATH}/files/")
+        verify_pkgvars(pkgvars_path)
+    finally:
+        copyfile(pkgvars_copy_path, pkgvars_path)
+        os.remove(pkgvars_copy_path)
+
+
+def verify_pkgvars(pkgvars_path):
+    with open(pkgvars_path, "r") as reader:
+        content = reader.read()
+        index = content.find(f"pkglatest={PROJECT_VERSION}.{PROJECT_NAME}-1")
+        assert index > -1
+
+
+def test_update_all_changes():
+    pkgvars_path = f"{TEST_BASE_PATH}/files/pkgvars"
+    pkgvars_copy_path = f"{pkgvars_path}_copy"
+    spec_file = f"{TEST_BASE_PATH}/files/{spec_file_name(PROJECT_NAME)}"
+    spec_file_copy = f"{spec_file}_copy"
+    spec_file_reference = f"{TEST_BASE_PATH}/files/{spec_file_name(PROJECT_NAME)}"
+
+    changelog_file_path = f"{TEST_BASE_PATH}/files/debian/changelog"
+    changelog_file_copy_path = f"{changelog_file_path}_copy"
+    copyfile(changelog_file_path, changelog_file_copy_path)
+    copyfile(pkgvars_path, pkgvars_copy_path)
+    copyfile(spec_file, spec_file_copy)
+
+    try:
+        package_properties_param = PackagePropertiesParams(project_name=PROJECT_NAME,
+                                                           project_version=PROJECT_VERSION, fancy=True,
+                                                           fancy_version_number=1,
+                                                           name_surname=NAME_SURNAME, microsoft_email=MICROSOFT_EMAIL,
+                                                           changelog_date=CHANGELOG_DATE)
+        update_all_changes(GITHUB_TOKEN, package_properties_param, TAG_NAME, f"{TEST_BASE_PATH}/files")
+        verify_prepend_debian_changelog(changelog_file_path)
+        verify_pkgvars(pkgvars_path)
+        verify_rpm_spec(spec_file_reference, spec_file)
+    finally:
+        copyfile(changelog_file_copy_path, changelog_file_path)
+        copyfile(pkgvars_copy_path, pkgvars_path)
+        copyfile(spec_file_copy, spec_file)
+
+        os.remove(changelog_file_copy_path)
+        os.remove(pkgvars_copy_path)
+        os.remove(spec_file_copy)
+
+
+def test_regex():
+    print(re.match(r"^### \w+\sv\d+\.\d+\.\d+\s\(\w+\s\d+,\s\d+\)\s###$", "### citus v10.0.3 (March 16, 2021) ###"))
