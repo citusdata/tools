72605a544dcd42cf69321956292f32a06aac296d
gindibay <gurkanindibay@gmail.com>
2021-04-01 12:18:05 +0300

    Fix bash command

--- bash/execute_update_properties.sh
+++ bash/execute_update_properties.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 python ../../python/update_package_properties.py --gh_token "${GITHUB_TOKEN}" --prj_name "${PRJ_NAME}" --prj_ver "${PRJ_VER}"  --tag_name "${TAG_NAME}" --fancy "${FANCY}" \
 --fancy_ver_no "${FANCY_VERSION_NO}" --email "${MICROSOFT_EMAIL}" --name "${NAME}" --date "$(date  '+%Y.%m.%d %H:%M:%S &z')" --exec-path "$(pwd)" &>2
 
