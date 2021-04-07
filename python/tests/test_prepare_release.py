25d7ebb0953fdfc3ac04ef7e09a44a00c44a2940
gindibay <gurkanindibay@gmail.com>
2021-04-07 17:56:20 +0300

    Add checkout for citus project for testing

--- python/tests/test_prepare_release.py
+++ python/tests/test_prepare_release.py
@@ -1,9 +1,11 @@
 import shutil
 import unittest
+import os
 
 import pathlib2
 
 from ..prepare_release import *
+from ..common_tool_methods import *
 
 github_token = os.getenv("GH_TOKEN")
 
@@ -14,15 +16,19 @@ CITUS_PROJECT_TEST_PATH_COPY = f"{TEST_BASE_PATH}/projects/citus_copy"
 
 class PrepareReleaseTestCases(unittest.TestCase):
     def test_major_release(self):
-        shutil.copytree(CITUS_PROJECT_TEST_PATH, CITUS_PROJECT_TEST_PATH_COPY)
-        try:
-            update_release(github_token=github_token, project_name="citus", project_version="10.1.0",
-                           main_branch="master",
-                           earliest_pr_date=datetime.strptime('2021.03.25 00:00', '%Y.%m.%d %H:%M'),
-                           exec_path=CITUS_PROJECT_TEST_PATH_COPY)
-        # current branch is release-$version number
-        # check for configure in
-        # check for
-        finally:
-            # shutil.rmtree(CITUS_PROJECT_TEST_PATH_COPY)
-            run(f"rm -r {CITUS_PROJECT_TEST_PATH_COPY}")
+        # shutil.copytree(CITUS_PROJECT_TEST_PATH, CITUS_PROJECT_TEST_PATH_COPY)
+        if not os.path.exists("citus"):
+            run("git clone https://github.com/citusdata/citus.git")
+        run("cd citus")
+        # try:
+        update_release(github_token=github_token, project_name="citus", project_version="10.1.0",
+                       main_branch="master",
+                       earliest_pr_date=datetime.strptime('2021.03.25 00:00', '%Y.%m.%d %H:%M'),
+                       exec_path=CITUS_PROJECT_TEST_PATH_COPY)
+
+    # current branch is release-$version number
+    # check for configure in
+    # check for
+    # finally:
+    #     # shutil.rmtree(CITUS_PROJECT_TEST_PATH_COPY)
+    #     run(f"rm -r {CITUS_PROJECT_TEST_PATH_COPY}")
