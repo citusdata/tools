015f247af599086eaaa35274fb2471e6937fc625
gindibay <gurkanindibay@gmail.com>
2021-04-29 10:31:57 +0300

    Finish upload_to_package_cloud unit tests

--- packaging_automation/tests/test_citus_package.py
+++ packaging_automation/tests/test_citus_package.py
@@ -4,6 +4,8 @@ import pathlib2
 from .test_utils import generate_new_gpg_key
 from ..citus_package import *
 from ..common_tool_methods import *
+from ..upload_to_package_cloud import upload_files_in_directory_to_package_cloud, delete_package_from_package_cloud, \
+    does_package_exist
 
 TEST_BASE_PATH = os.getenv("BASE_PATH", default=pathlib2.Path(__file__).parents[2])
 
@@ -20,6 +22,8 @@ TEST_GPG_KEY_NAME = "Citus Data <packaging@citusdata.com>"
 TEST_GPG_KEY_PASSPHRASE = os.getenv("PACKAGING_PASSPHRASE")
 GH_TOKEN = os.getenv("GH_TOKEN")
 PLATFORM = os.getenv("PLATFORM")
+PACKAGECLOUD_API_TOKEN = os.getenv("PACKAGE_CLOUD_API_TOKEN")
+REPO_CLIENT_SECRET = os.getenv("REPO_CLIENT_SECRET")
 
 
 def setup_module():
@@ -51,3 +55,26 @@ def test_build_packages():
     release_output_folder = f"{BASE_OUTPUT_FOLDER}/{sub_folder}"
     delete_gpg_key_by_name(TEST_GPG_KEY_NAME)
     assert len(os.listdir(release_output_folder)) == package_counts[platform]
+
+
+def test_upload_to_package_cloud():
+    output = upload_files_in_directory_to_package_cloud(BASE_OUTPUT_FOLDER, PLATFORM, PACKAGECLOUD_API_TOKEN,
+                                                        "sample")
+    distro_parts = PLATFORM.split("/")
+    if len(distro_parts) != 2:
+        raise ValueError("Platform should consist of 2 parts splitted with '/'")
+    for return_value in output.return_values:
+        exists = does_package_exist(PACKAGECLOUD_API_TOKEN, "citus-bot", "sample",
+                                    os.path.basename(return_value.file_name),
+                                    PLATFORM)
+        if not exists:
+            raise ValueError(f"{os.path.basename(return_value.file_name)} could not be found on package cloud")
+
+    for return_value in output.return_values:
+        delete_output = delete_package_from_package_cloud(PACKAGECLOUD_API_TOKEN, "citus-bot", "sample",
+                                                          distro_parts[0], distro_parts[1],
+                                                          os.path.basename(return_value.file_name))
+        if delete_output.success_status:
+            print(f"{os.path.basename(return_value.file_name)} deleted successfully")
+        else:
+            print(f"{os.path.basename(return_value.file_name)} can not be deleted. Message: {delete_output.message}")
