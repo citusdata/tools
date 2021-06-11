bec9a89aac10df2d23a5f67d1876a07e3191878e
Gurkan Indibay <gurkanindibay@gmail.com>
2021-06-11 06:24:08 +0000

    Organize import statements

--- packaging_automation/tests/test_update_docker.py
+++ packaging_automation/tests/test_update_docker.py
@@ -3,26 +3,27 @@ import os
 import pathlib2
 from datetime import datetime
 
-from .. import update_docker
-from .. import common_tool_methods
+from ..update_docker import (update_docker_file_for_latest_postgres, update_regular_docker_compose_file,
+                             update_docker_file_alpine, update_docker_file_for_postgres12, update_changelog)
+from ..common_tool_methods import (run, get_version_details)
 
 BASE_PATH = os.getenv("BASE_PATH", default=pathlib2.Path(__file__).parents[2])
 TEST_BASE_PATH = f"{BASE_PATH}/docker"
 PROJECT_VERSION = "10.0.3"
 PROJECT_NAME = "citus"
-version_details = common_tool_methods.get_version_details(PROJECT_VERSION)
+version_details = get_version_details(PROJECT_VERSION)
 TEMPLATE_PATH = f"{BASE_PATH}/packaging_automation/templates/docker"
 
 
 def setup_module():
     if not os.path.exists("docker"):
-        common_tool_methods.run("git clone https://github.com/citusdata/docker.git")
+        run("git clone https://github.com/citusdata/docker.git")
 
 
 def teardown_module():
     if os.path.exists("docker"):
-        common_tool_methods.run("chmod -R 777 docker")
-        common_tool_methods.run("sudo rm -rf docker")
+        run("chmod -R 777 docker")
+        run("sudo rm -rf docker")
 
 
 def file_controls(file_name: str):
@@ -36,7 +37,7 @@ def file_controls(file_name: str):
 
 
 def test_update_docker_file_for_latest_postgres():
-    update_docker.update_docker_file_for_latest_postgres(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
+    update_docker_file_for_latest_postgres(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
     with open(f"{TEST_BASE_PATH}/Dockerfile", "r") as reader:
         content = reader.read()
         lines = content.splitlines()
@@ -47,7 +48,7 @@ def test_update_docker_file_for_latest_postgres():
 
 
 def test_update_regular_docker_compose_file():
-    update_docker.update_regular_docker_compose_file(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
+    update_regular_docker_compose_file(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
     parameterized_str = f"    image: 'citusdata/{PROJECT_NAME}:{PROJECT_VERSION}'"
     with open(f"{TEST_BASE_PATH}/docker-compose.yml", "r") as reader:
         content = reader.read()
@@ -58,7 +59,7 @@ def test_update_regular_docker_compose_file():
 
 
 def test_update_docker_file_alpine():
-    update_docker.update_docker_file_alpine(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
+    update_docker_file_alpine(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
     with open(f"{TEST_BASE_PATH}/alpine/Dockerfile", "r") as reader:
         content = reader.read()
         lines = content.splitlines()
@@ -67,7 +68,7 @@ def test_update_docker_file_alpine():
 
 
 def test_update_docker_file_for_postgres12():
-    update_docker.update_docker_file_for_postgres12(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
+    update_docker_file_for_postgres12(PROJECT_VERSION, TEMPLATE_PATH, TEST_BASE_PATH)
     with open(f"{TEST_BASE_PATH}/postgres-12/Dockerfile", "r") as reader:
         content = reader.read()
         lines = content.splitlines()
@@ -78,7 +79,7 @@ def test_update_docker_file_for_postgres12():
 
 
 def test_update_changelog():
-    update_docker.update_changelog(PROJECT_VERSION, TEST_BASE_PATH)
+    update_changelog(PROJECT_VERSION, TEST_BASE_PATH)
     with open(f"{TEST_BASE_PATH}/CHANGELOG.md", "r") as reader:
         content = reader.read()
         lines = content.splitlines()
