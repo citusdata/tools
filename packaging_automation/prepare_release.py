283854a9d4a460db49ce91b8d9a4ad852832f65e
gindibay <gurkanindibay@gmail.com>
2021-06-16 17:50:23 +0300

    Fix for issue #113

--- packaging_automation/prepare_release.py
+++ packaging_automation/prepare_release.py
@@ -1,18 +1,17 @@
+import argparse
 import os
 import uuid
 from dataclasses import dataclass
 from datetime import datetime
-import argparse
-from enum import Enum
 
 import pathlib2
 from github import Github, Repository
 from parameters_validation import (non_blank, non_empty)
 
-from .common_tool_methods import (get_version_details, get_upcoming_patch_version, is_major_release,
+from .common_tool_methods import (get_version_details, is_major_release,
                                   get_prs_for_patch_release,
                                   filter_prs_by_label, cherry_pick_prs, run, replace_line_in_file, get_current_branch,
-                                  find_nth_matching_line_and_line_number, get_minor_version, get_patch_version_regex,
+                                  find_nth_matching_line_and_line_number, get_patch_version_regex,
                                   remote_branch_exists, local_branch_exists, prepend_line_in_file,
                                   get_template_environment, get_upcoming_minor_version, remove_cloned_code)
 from .common_validations import (CITUS_MINOR_VERSION_PATTERN, CITUS_PATCH_VERSION_PATTERN, is_version)
@@ -480,7 +479,7 @@ def create_new_sql_for_upgrade_path(current_schema_version, distributed_dir_path
     newly_created_sql_file = upgrade_sql_file_name(current_schema_version, upcoming_minor_version)
     print(f"### Creating upgrade file {newly_created_sql_file}... ###")
     with open(f"{distributed_dir_path}/{newly_created_sql_file}", "w") as f_writer:
-        content = f"/* citus--{current_schema_version}--{upcoming_minor_version}-1 */"
+        content = f"-- citus--{current_schema_version}--{upcoming_minor_version}-1"
         content = content + "\n\n"
         content = content + f"-- bump version to {upcoming_minor_version}-1" + "\n\n"
         f_writer.write(content)
@@ -493,7 +492,7 @@ def create_new_sql_for_downgrade_path(current_schema_version, distributed_dir_pa
     newly_created_sql_file = f"citus--{upcoming_minor_version}-1--{current_schema_version}.sql"
     print(f"### Creating downgrade file {newly_created_sql_file}... ###")
     with open(f"{distributed_dir_path}/{newly_created_sql_file}", "w") as f_writer:
-        content = f"/* citus--{upcoming_minor_version}-1--{current_schema_version} */"
+        content = f"-- citus--{upcoming_minor_version}-1--{current_schema_version}"
         content = content + "\n"
         content = (
                 content + f"-- this is an empty downgrade path since "
