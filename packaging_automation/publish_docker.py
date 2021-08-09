a0440b5f8f776d7decc758b2e278a56a720d9515
gindibay <gurkanindibay@gmail.com>
2021-08-09 18:46:56 +0300

    * Fixes current branch problem for tag push * Adds some logs to track script execution

--- packaging_automation/publish_docker.py
+++ packaging_automation/publish_docker.py
@@ -1,5 +1,5 @@
 import argparse
-import os.path
+import os
 from enum import Enum
 from typing import Tuple, List
 
@@ -7,7 +7,7 @@ import docker
 import pathlib2
 from parameters_validation import validate_parameters
 
-from .common_tool_methods import remove_prefix
+from .common_tool_methods import remove_prefix, get_current_branch
 from .common_validations import is_tag
 
 BASE_PATH = pathlib2.Path(__file__).parents[1]
@@ -92,7 +92,7 @@ def get_image_tag(tag_prefix: str, docker_image_type: DockerImageType) -> str:
     return f"{tag_prefix}{tag_suffix}"
 
 
-def publish_docker_image_on_push(docker_image_type: DockerImageType, github_ref: str, current_branch: str, ):
+def publish_docker_image_on_push(docker_image_type: DockerImageType, github_ref: str, current_branch: str):
     triggering_event_info, resource_name = decode_triggering_event_info(github_ref)
     for docker_image_type in regular_images_to_be_built(docker_image_type):
         if triggering_event_info == GithubTriggerEventSource.branch_push:
@@ -112,7 +112,6 @@ def publish_docker_image_on_schedule(docker_image_type: DockerImageType, current
 def publish_docker_image_manually(manual_trigger_type_param: ManualTriggerType, current_branch: str,
                                   docker_image_type: DockerImageType, tag_name: str = "") -> None:
     if manual_trigger_type_param == ManualTriggerType.main and not tag_name:
-        print(f"Manually building main image for {docker_image_type.name}   ")
         for it in regular_images_to_be_built(docker_image_type):
             publish_main_docker_images(it, current_branch)
     elif manual_trigger_type_param == ManualTriggerType.tags and tag_name:
@@ -122,43 +121,60 @@ def publish_docker_image_manually(manual_trigger_type_param: ManualTriggerType, 
         publish_nightly_docker_image()
 
 
-
-
 def publish_main_docker_images(docker_image_type: DockerImageType, current_branch: str):
-    print(f"Building main docker image for {docker_image_type.name}")
+    print(f"Building main docker image for {docker_image_type.name}...")
     docker_image_name = f"{DOCKER_IMAGE_NAME}:{docker_image_type.name}"
     docker_client.images.build(dockerfile=docker_image_info_dict[docker_image_type]['file-name'],
                                tag=docker_image_name,
                                path=".")
+    print(f"Main docker image for {docker_image_type.name} built.")
     if current_branch == DEFAULT_BRANCH_NAME:
+        print(f"Publishing main docker image for {docker_image_type.name}...")
         docker_client.images.push(DOCKER_IMAGE_NAME, tag=docker_image_type.name)
+        print(f"Publishing main docker image for {docker_image_type.name} finished")
+    else:
+        print(
+            f"Since current branch {current_branch} is not equal to "
+            f"{DEFAULT_BRANCH_NAME} {docker_image_name} will not be pushed.")
 
 
 def publish_tagged_docker_images(docker_image_type, tag_name: str, current_branch: str):
-    print(f"Building and publishing tagged image {docker_image_type.name} for tag {tag_name}")
+    print(f"Building and publishing tagged image {docker_image_type.name} for tag {tag_name}...")
     tag_parts = decode_tag_parts(tag_name)
     tag_version_part = ""
     docker_image_name = f"{DOCKER_IMAGE_NAME}:{docker_image_type.name}"
     docker_client.images.build(dockerfile=docker_image_info_dict[docker_image_type]['file-name'],
                                tag=docker_image_name,
                                path=".")
+    if current_branch != DEFAULT_BRANCH_NAME:
+        print(f"Since current branch {current_branch} is not equal to {DEFAULT_BRANCH_NAME} tags will not be pushed.")
+    print(f"{docker_image_type.name} image built.Now starting tagging and pushing...")
     for tag_part in tag_parts:
         tag_version_part = tag_version_part + tag_part
         image_tag = get_image_tag(tag_version_part, docker_image_type)
-        print(f"Tagging {docker_image_name} with the tag {image_tag}")
+        print(f"Tagging {docker_image_name} with the tag {image_tag}...")
         docker_api_client.tag(docker_image_name, docker_image_name, image_tag)
+        print(f"Tagging {docker_image_name} with the tag {image_tag} finished.")
+
         if current_branch == DEFAULT_BRANCH_NAME:
+            print(f"Pushing {docker_image_name} with the tag {image_tag}...")
             docker_client.images.push(DOCKER_IMAGE_NAME, tag=image_tag)
+            print(f"Pushing {docker_image_name} with the tag {image_tag} finished")
+
         tag_version_part = tag_version_part + "."
+    print(f"Building and publishing tagged image {docker_image_type.name} for tag {tag_name} finished.")
 
 
 def publish_nightly_docker_image():
-    print("Building nightly image for citus")
+    print("Building nightly image...")
     docker_image_name = f"{DOCKER_IMAGE_NAME}:{docker_image_info_dict[DockerImageType.nightly]['docker-tag']}"
     docker_client.images.build(dockerfile=docker_image_info_dict[DockerImageType.nightly]['file-name'],
                                tag=docker_image_name,
                                path=".")
+    print("Nightly image build finished.")
+    print("Pushing nightly image...")
     docker_client.images.push(DOCKER_IMAGE_NAME, tag=docker_image_info_dict[DockerImageType.nightly]['docker-tag'])
+    print("Nightly image push finished.")
 
 
 def validate_and_extract_general_parameters(docker_image_type_param: str, pipeline_trigger_type_param: str) -> Tuple[
@@ -207,7 +223,7 @@ if __name__ == "__main__":
 
     pipeline_trigger_type, image_type = validate_and_extract_general_parameters(args.image_type,
                                                                                 args.pipeline_trigger_type)
-    triggering_event_info, current_branch = decode_triggering_event_info(args.github_ref)
+    current_branch = get_current_branch(os.getcwd())
     if pipeline_trigger_type == GithubPipelineTriggerType.workflow_dispatch:
         manual_trigger_type = validate_and_extract_manual_exec_params(args.manual_trigger_type, args.tag_name)
         publish_docker_image_manually(manual_trigger_type_param=manual_trigger_type, current_branch=current_branch,
