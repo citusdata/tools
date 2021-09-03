8ba74b62e26fd63b78da936d214c9de86165f6fe
gindibay <gurkanindibay@gmail.com>
2021-09-03 12:17:16 +0300

    Fixes static code analysis issues

--- packaging_automation/upload_to_package_cloud.py
+++ packaging_automation/upload_to_package_cloud.py
@@ -9,6 +9,8 @@ import pathlib2
 import requests
 from requests.auth import HTTPBasicAuth
 
+from release.tools.packaging_automation.common_tool_methods import DEFAULT_ENCODING_FOR_FILE_HANDLING
+
 supported_distros = {
     "el/7": 140,
     "el/8": 205,
@@ -53,19 +55,20 @@ BASE_PATH = pathlib2.Path(__file__).parents[1]
 
 def upload_to_package_cloud(distro_name, package_name, package_cloud_token, repo_name) -> ReturnValue:
     distro_id = supported_distros[distro_name]
+    with open(package_name, "r", encoding=DEFAULT_ENCODING_FOR_FILE_HANDLING,
+              errors=DEFAULT_ENCODING_FOR_FILE_HANDLING) as file_handle:
+        files = {
+            'package[distro_version_id]': (None, str(distro_id)),
+            'package[package_file]': (
+                package_name, file_handle),
+        }
 
-    files = {
-        'package[distro_version_id]': (None, str(distro_id)),
-        'package[package_file]': (
-            package_name, open(package_name, 'rb')),
-    }
-
-    package_query_url = (
-        f'https://{package_cloud_token}:@packagecloud.io/api/v1/repos/{repo_name}/packages.json')
-    print(f"Uploading package {os.path.basename(package_name)}")
-    response = requests.post(package_query_url, files=files)
-    print(f"Response from package cloud: {response.content}")
-    return ReturnValue(response.ok, response.content.decode("ascii"), package_name, distro_name, repo_name)
+        package_query_url = (
+            f'https://{package_cloud_token}:@packagecloud.io/api/v1/repos/{repo_name}/packages.json')
+        print(f"Uploading package {os.path.basename(package_name)}")
+        response = requests.post(package_query_url, files=files)
+        print(f"Response from package cloud: {response.content}")
+        return ReturnValue(response.ok, response.content.decode("ascii"), package_name, distro_name, repo_name)
 
 
 def upload_files_in_directory_to_package_cloud(directoryName: str, distro_name: str, package_cloud_token: str,
