0cdb4d6e0cb3c88f16beccb9e49bce8ab405122b
gindibay <gurkanindibay@gmail.com>
2021-09-30 08:32:52 +0300

    Changes xenial package count since debug package does not exist

--- packaging_automation/tests/test_citus_package.py
+++ packaging_automation/tests/test_citus_package.py
@@ -30,7 +30,7 @@ single_postgres_package_counts = {
     "debian/stretch": 2,
     "debian/buster": 2,
     "debian/bullseye": 2,
-    "ubuntu/xenial": 2,
+    "ubuntu/xenial": 1,
     "ubuntu/bionic": 2,
     "ubuntu/focal": 2
 }
