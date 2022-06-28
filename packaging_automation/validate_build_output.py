6bc4b762d6544fd1d5f1e121cc28a0b90ea27a06
gindibay <gurkanindibay@gmail.com>
2022-06-28 09:46:25 +0300

    Adds procedure to be able to validate warnings

---
+++ packaging_automation/validate_build_output.py
@@ -0,0 +1,15 @@
+import argparse
+
+from .packaging_warning_handler import (validate_output)
+from .common_tool_methods import (PackageType)
+from pathlib import Path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_file', required=True)
+    parser.add_argument('--ignore_file', required=True)
+    parser.add_argument('--package_type', choices=[p.name for p in PackageType], required=True)
+
+    args = parser.parse_args()
+    build_output = Path(args.output_file).read_text()
+    validate_output(build_output, args.ignore_file, PackageType[args.package_type])
