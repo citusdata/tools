bdc469d8b8da77be9b868327335d4c0608638a60
Gurkan Indibay <gindibay@microsoft.com>
2023-03-21 13:07:26 +0300

    Adds pypi statistics collector

--- packaging_automation/pypi_django_mt_stats_collector.py
+++ packaging_automation/pypi_django_mt_stats_collector.py
@@ -1,62 +1,83 @@
-import requests
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import date, timedelta
+from datetime import date, timedelta,datetime
+import pypistats
+import json
+from pprint import pprint
+from .dbconfig import Base, DbParams, db_session
+import os
+
 
 # Define the database connection
-db_name = "citus-stats"
-db_user = "citus_admin@citus-stats"
-db_password = "CtsGreat1923"
-db_host = "citus-stats.postgres.database.azure.com"
-db_port = "5432"
-engine = create_engine(
-    f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+db_name = os.getenv(
+    "DB_NAME",
 )
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
+db_user = os.getenv("DB_USER_NAME")
+db_password = os.getenv("DB_PASSWORD")
+db_host_and_port = os.getenv("DB_HOST_AND_PORT")
+
+db_params = DbParams(
+    user_name=db_user,
+    password=db_password,
+    host_and_port=db_host_and_port,
+    db_name=db_name,
+)
 
 
 # Define the model for the download numbers
 class DownloadNumbers(Base):
-    __table_name__ = "pypi_downloads"
+    __tablename__ = "pypi_downloads"
 
     id = Column(Integer, primary_key=True)
     fetch_date = Column(Date)
     library_name = Column(String)
-    download_count = Column(Integer)
+    download_count = Column(String)
     download_date = Column(Date)
 
 
-# Retrieve the download numbers from PyPI for the last 7 days
-package_name = "django-multitenant"
-days_to_retrieve = 7
-dates = [date.today() - timedelta(days=i) for i in range(days_to_retrieve)]
-download_numbers = []
-for date in dates:
-    url = f"https://pypistats.org/api/packages/{package_name}/{date.isoformat()}/last"
-    response = requests.get(url)
-    download_numbers.extend(response.json()["data"])
+def fetch_download_numbers(package_name):
 
-# Save the download numbers to the database
-session = Session()
-for downloads in download_numbers:
-    existing_record = (
-        session.query(DownloadNumbers)
-        .filter_by(
-            library_name=package_name,
-            downloads=downloads["downloads"],
-            date=downloads["date"],
-        )
-        .first()
+    print(f"Fetching download numbers for {package_name} from pypi.org. Started at {datetime.now()}")
+    download_numbers = json.loads(
+        pypistats.overall(package_name, format="json", mirrors=True, total=True)
     )
-    if not existing_record:
-        record = DownloadNumbers(
-            fetch_date=date.today(),
-            library_name=package_name,
-            downloads=downloads["downloads"],
-            date=downloads["date"],
-        )
-        session.add(record)
+    session = db_session(db_params=db_params, is_test=False, create_db_objects=True)
+    print(f"{len(download_numbers['data'])} records fetched from pypi.org. Starting to add to database. Started at {datetime.now()}")
+    
 
-session.commit()
+    new_record_count = 0
+    existing_record_count = 0
+    for downloads in download_numbers["data"]:
+
+        existing_record = (
+            session.query(DownloadNumbers)
+            .filter_by(
+                library_name=package_name,
+                download_count=downloads["downloads"],
+                download_date=downloads["date"],
+            )
+            .first()
+        )
+        if not existing_record:
+            new_record_count += 1
+            print(f"Adding {package_name} {downloads['downloads']} {downloads['date']}")
+            record = DownloadNumbers(
+                fetch_date=date.today(),
+                library_name=package_name,
+                download_count=downloads["downloads"],
+                download_date=downloads["date"],
+            )
+            session.add(record)
+        else:
+            existing_record_count += 1
+                   
+            
+
+    session.commit()
+    print(f"Process finished. New records: {new_record_count} Existing records: {existing_record_count}. Finished at {datetime.now()}")
+
+
+packages = ["django-multitenant"]
+for package_name in packages:
+    fetch_download_numbers(package_name)
