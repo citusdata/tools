01264adeedfb93d17d6c0cdde135eca735262b60
Seda Gündoğdu <69769369+sedagundogdu@users.noreply.github.com>
2024-05-25 12:39:43 +0300

    Updates python package versions (#354)
    * versions updated
    
    * code format fixes
    
    * convert offset-naive objects to offset-aware
    
    * adding missing part of convertion

--- packaging_automation/tests/test_common_tool_methods.py
+++ packaging_automation/tests/test_common_tool_methods.py
@@ -3,6 +3,9 @@ import uuid
 from datetime import datetime
 from shutil import copyfile
 
+from datetime import timezone
+
+
 import pathlib2
 from github import Github
 
@@ -265,9 +268,9 @@ def test_getprs():
     repository = g.get_repo("citusdata/citus")
     prs = get_prs_for_patch_release(
         repository,
-        datetime.strptime("2021.02.26", "%Y.%m.%d"),
+        datetime.strptime("2021.02.26", "%Y.%m.%d").replace(tzinfo=timezone.utc),
         "master",
-        datetime.strptime("2021.03.02", "%Y.%m.%d"),
+        datetime.strptime("2021.03.02", "%Y.%m.%d").replace(tzinfo=timezone.utc),
     )
     assert len(prs) == 6
     assert prs[0].number == 4748
@@ -278,9 +281,9 @@ def test_getprs_with_backlog_label():
     repository = g.get_repo("citusdata/citus")
     prs = get_prs_for_patch_release(
         repository,
-        datetime.strptime("2021.02.20", "%Y.%m.%d"),
+        datetime.strptime("2021.02.20", "%Y.%m.%d").replace(tzinfo=timezone.utc),
         "master",
-        datetime.strptime("2021.02.27", "%Y.%m.%d"),
+        datetime.strptime("2021.02.27", "%Y.%m.%d").replace(tzinfo=timezone.utc),
     )
     prs_backlog = filter_prs_by_label(prs, "backport")
     assert len(prs_backlog) == 1
