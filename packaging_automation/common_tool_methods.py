a9608ccdefe6d5cbba0090767c63cb6a2de3942d
Seda Gundogdu <gundogduseda00@gmail.com>
2025-01-20 17:35:25 +0300

    Add noble to supported ubuntu releases

--- packaging_automation/common_tool_methods.py
+++ packaging_automation/common_tool_methods.py
@@ -37,11 +37,11 @@ DEFAULT_UNICODE_ERROR_HANDLER = "surrogateescape"
 referenced_repos: List[Repo] = []
 
 supported_platforms = {
-    "debian": ["bookworm", "bullseye", "buster", "stretch", "jessie", "wheezy", "noble"],
+    "debian": ["bookworm", "bullseye", "buster", "stretch", "jessie", "wheezy"],
     "almalinux": ["8", "9"],
     "el": ["9", "8", "7", "6"],
     "ol": ["9", "8", "7"],
-    "ubuntu": ["focal", "bionic", "trusty", "jammy", "kinetic"],
+    "ubuntu": ["focal", "bionic", "trusty", "jammy", "kinetic", "noble"],
 }
 
 
